1fe7f8a3edc3034e67fb713a0fe0a9d01a607315
Mohamed25092003 <mohamednaaguib@gmail.com>
2026-05-05 20:14:01 +0300

    checking alias found no need to correction

--- controllers/Preprocessing.py
+++ controllers/Preprocessing.py
@@ -4,9 +4,24 @@ import json
 class Preprocessing(BaseController):
     def __init__(self):
         super().__init__()
-    def read_json(self):
-       with open(self.job_offers_path, 'r') as f:
-           job_offers = json.load(f)
-       with open(self.candidates_path, 'r') as f:
-              candidates = json.load(f)
-       return job_offers, candidates
+    def read_json(self,text_candidates=None,text_jobs=None):
+        if text_jobs is not None:
+            job_offers = json.loads(text_jobs)
+        if text_candidates is not None:
+            candidates = json.loads(text_candidates)
+
+        return job_offers, candidates
+
+    def Alias_correction(self, text):
+        # Define a mapping of common aliases to their corrected forms
+        alias_mapping = {
+            "JS": "JavaScript",
+            "TS": "TypeScript",
+            "ReactJS": "React",
+            "Node": "Node.js"
+        }
+
+        corrected_words = [alias_mapping.get(word, word) for word in text.split()]
+       
+        corrected_text = ' '.join(corrected_words)
+        return corrected_text
--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 from controllers import Preprocessing
 
 if __name__ == "__main__":
-    jobs, candidates = Preprocessing().read_json()
+    # Read the JSON files
+    with open('./Data/jobs.json', 'r') as f:
+        text_jobs = f.read()
+    with open('./Data/candidates.json', 'r') as f:
+        text_candidates = f.read()
+    count=0
+    aliases = ["JS", "TS", "ReactJS", "Node"]
+    for job in text_jobs:
+        if any(alias in job for alias in aliases):
+            count+=1
+    print(f"Number of job offers containing aliases: {count}")
+    for candidate in text_candidates:
+        if any(alias in candidate for alias in aliases):
+            count+=1
+    print(f"Number of candidates containing aliases: {count}")
+            
+    # Create an instance of the Preprocessing class
+    preprocessing = Preprocessing()
+    # Read and process the JSON data
+    corrected_candidates = preprocessing.Alias_correction(text_candidates)
+    corrected_jobs = preprocessing.Alias_correction(text_jobs)
+    for job in corrected_jobs:
+        if any(alias in job for alias in aliases):
+            count+=1
+    print(f"Number of job offers containing aliases after correction: {count}")
+    for candidate in corrected_candidates:
+        if any(alias in candidate for alias in aliases):
+            count+=1
+    print(f"Number of candidates containing aliases after correction: {count}")
+    # # print("Corrected Candidates:\n", corrected_candidates)
+    # print("Corrected Job Offers:\n", corrected_jobs)
     
